0cfd06064c6e745a05ddfb9e75ef4c6ca42a1320
Rein van der Woerd <reinvanderwoerd@me.com>
2021-12-06 17:08:25 +0100

    img

--- src/main/kotlin/simple.kt
+++ src/main/kotlin/simple.kt
@@ -1,5 +1,6 @@
 import org.openrndr.applicationAsync
 import org.openrndr.color.ColorRGBa
+import org.openrndr.draw.loadImageSuspend
 import org.openrndr.webgl.Screenshots
 
 
@@ -10,6 +11,8 @@ suspend fun main() = applicationAsync {
     }
 
     program {
+        val img = loadImageSuspend("./Bucephala-albeola-010.jpg")
+
         mouse.buttonDown.listen {
             console.log("mouse down")
 //            clipboard.contents = seconds.toString()
@@ -38,7 +41,12 @@ suspend fun main() = applicationAsync {
             val a = ColorRGBa.BLUE
             drawer.clear(a)
             drawer.fill = ColorRGBa.WHITE
+
+            drawer.image(img, 200.0, 0, 200.0, 200.0)
+
             drawer.rectangle(10.0,10.0, 200.0,200.0)
+            drawer.circle(10.0 + 100.0, 300.0, 100.0)
+
         }
     }
 }
